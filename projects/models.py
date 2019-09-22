fa028819dbc26f65a7161871bde7a954ae3e05ca
Mahesh <2017.mahesh.keswani@ves.ac.in>
2019-09-22 13:48:40 +0530

    Apps like projects and blogs added

---
+++ projects/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Project(models.Model):
+
+	image = models.ImageField(upload_to='images/')
+	title = models.CharField(max_length=100)
+	description = models.CharField(max_length=300)
+	
